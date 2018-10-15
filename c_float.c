98a257186cd24854714798a4b71b66d90216259d
Adrian Mucha <kestarumper8@gmail.com>
2018-10-15 12:35:59 +0200

    fetch MAX from float.h

--- c_float.c
+++ c_float.c
@@ -4,5 +4,7 @@
 int main(int argc, char const *argv[]) {
   printf("eps(FLOAT32) = %e\n", __FLT_EPSILON__);
   printf("eps(FLOAT64) = %e\n", __FLT64_EPSILON__);
+  printf("max(FLOAT32) = %e\n", __FLT_MAX__);
+  printf("max(FLOAT64) = %e\n", __FLT64_MAX__);
   return 0;
 }
