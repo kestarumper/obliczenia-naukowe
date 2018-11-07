5dcd1ceff515d77031e643e6b012920aa1542e4f
Adrian Mucha <kestarumper8@gmail.com>
2018-11-07 11:51:34 +0100

    implement zadanie 4

---
+++ lista2/zadanie4.jl
@@ -0,0 +1,39 @@
+using Polynomials
+
+function evalPolynomials(cfs)
+      coefficients = reverse(cfs)
+      P = Poly(coefficients)
+      p = poly(1:20)
+      pierwiastki = roots(P)
+
+      pad = 26
+      for k in 20:-1:1
+            zk = pierwiastki[20 - k + 1]
+            valP = abs(P(zk))
+            valp = abs(p(zk))
+            h = abs(zk - k)
+            println(
+                  rpad("|P($zk)|", pad),
+                  rpad("= $valP", pad),
+                  rpad("|p($zk)|", pad),
+                  rpad("= $valp", pad),
+                  rpad("|$zk - $k|", pad),
+                  "= $h"
+            )
+      end
+end
+
+cfnts = [1, -210.0, 20615.0,-1256850.0,
+      53327946.0,-1672280820.0, 40171771630.0, -756111184500.0,
+      11310276995381.0, -135585182899530.0,
+      1307535010540395.0,     -10142299865511450.0,
+      63030812099294896.0,     -311333643161390640.0,
+      1206647803780373360.0,     -3599979517947607200.0,
+      8037811822645051776.0,      -12870931245150988800.0,
+      13803759753640704000.0,      -8752948036761600000.0,
+      2432902008176640000.0]
+println("")
+evalPolynomials(cfnts)
+println("Change -210 → -210-2^{-23}")
+cfnts[2] = -210 - 2.0^(-23)
+evalPolynomials(cfnts)
