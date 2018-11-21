45576e106a2fcbe0d27b1a55356a07340059d319
Adrian Mucha <kestarumper8@gmail.com>
2018-11-21 10:40:54 +0100

    modify mstycznych docstring

--- lista3/zad2.jl
+++ lista3/zad2.jl
@@ -1,24 +1,22 @@
 """
-Opis:
+# Opis:
 Funkcja oblicza pierwiastki funkcji używając metody stycznych.
 
-Dane:
-f, pf – funkcją f(x) oraz pochodną f
-0
-(x) zadane jako anonimowe funkcje,
-x0 – przybliżenie początkowe,
-delta,epsilon – dokładności obliczeń,
-maxit – maksymalna dopuszczalna liczba iteracji,
+# Dane:
+* f, pf – funkcją f(x) oraz pochodną f'(x) zadane jako anonimowe funkcje,
+* x0 – przybliżenie początkowe,
+* delta,epsilon – dokładności obliczeń,
+* maxit – maksymalna dopuszczalna liczba iteracji,
 
-Wyniki:
-(r,v,it,err) – czwórka, gdzie
-r – przybliżenie pierwiastka równania f(x) = 0,
-v – wartość f(r),
-it – liczba wykonanych iteracji,
-err – sygnalizacja błędu
-    0 - metoda zbieżna
-    1 - nie osiągnięto wymaganej dokładności w maxit iteracji,
-    2 - pochodna bliska zeru
+# Wyniki:
+* (r,v,it,err) – czwórka, gdzie
+* r – przybliżenie pierwiastka równania f(x) = 0,
+* v – wartość f(r),
+* it – liczba wykonanych iteracji,
+* err – sygnalizacja błędu
+    * 0 - metoda zbieżna
+    * 1 - nie osiągnięto wymaganej dokładności w maxit iteracji,
+    * 2 - pochodna bliska zeru
 """
 function mstycznych(f,pf,x0::Float64, delta::Float64, epsilon::Float64, maxit::Int)
     v = f(x0)
