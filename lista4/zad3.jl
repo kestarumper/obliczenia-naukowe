169b065a87ee19ade6f4dc2695d8a29e12a7107f
Adrian Mucha <kestarumper8@gmail.com>
2018-12-07 23:36:25 +0100

    init zad3

---
+++ lista4/zad3.jl
@@ -0,0 +1,12 @@
+"""
+
+"""
+function naturalna(x::Vector{Float64}, c::Vector{Float64}, t::Float64)
+    n = length(x)
+    if n != length(c)
+        return error("Lengths does not match")
+    end
+end
+
+x = Float64[5, -7, -6, 0]
+y = Float64[1, 2, 3, 4]
