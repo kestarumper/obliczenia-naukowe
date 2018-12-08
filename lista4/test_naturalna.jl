8cd503f8947cf0d15e783864e5daa6af509e18ee
Adrian Mucha <kestarumper8@gmail.com>
2018-12-08 16:25:41 +0100

    add tests for naturalna, finalize zad3

---
+++ lista4/test_naturalna.jl
@@ -0,0 +1,9 @@
+push!(LOAD_PATH, ".")
+using Ilorazy
+using Base.Test
+
+x = Float64[5, -7, -6, 0]
+fx = Float64[1, 2, 3, 4]
+@testset "Testy jednostkowe funkcji naturalna" begin
+    @test Ilorazy.naturalna(x, fx) ≈ [-954.0, -84.0, 35.0, 4.0]
+end
--- lista4/zad3.jl
+++ lista4/zad3.jl
@@ -1,12 +1,27 @@
 """
-
+# Dane:
+    x – wektor długości n + 1 zawierający węzły x0, . . . , xn
+    x[1]=x0,..., x[n+1]=xn
+    fx – wektor długości n + 1 zawierający ilorazy różnicowe
+    fx[1]=f[x0],
+    fx[2]=f[x0, x1],..., fx[n]=f[x0, . . . , xn−1], fx[n+1]=f[x0, . . . , xn]
+# Wyniki:
+    a – wektor długości n + 1 zawierający obliczone współczynniki postaci naturalnej
+    a[1]=a0,
+    a[2]=a1,..., a[n]=an−1, a[n+1]=an.
 """
-function naturalna(x::Vector{Float64}, c::Vector{Float64}, t::Float64)
+function naturalna(x::Vector{Float64}, c::Vector{Float64})
     n = length(x)
+    a = zeros(n)
     if n != length(c)
         return error("Lengths does not match")
     end
+    a[n] = c[n]
+    for k in n-1:-1:1
+        a[k] += c[k]
+        for i in k:n-1
+            a[i] -= a[i+1]*x[k]
+        end
+    end
+    return a
 end
-
-x = Float64[5, -7, -6, 0]
-y = Float64[1, 2, 3, 4]
