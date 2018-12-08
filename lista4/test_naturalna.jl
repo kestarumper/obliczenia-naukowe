026c264aeecf770409301a3c1bd822991f539511
Adrian Mucha <kestarumper8@gmail.com>
2018-12-08 18:13:50 +0100

    add smal test

--- lista4/test_naturalna.jl
+++ lista4/test_naturalna.jl
@@ -6,4 +6,5 @@ x = Float64[5, -7, -6, 0]
 fx = Float64[1, 2, 3, 4]
 @testset "Testy jednostkowe funkcji naturalna" begin
     @test Ilorazy.naturalna(x, fx) ≈ [-954.0, -84.0, 35.0, 4.0]
+    @test Ilorazy.naturalna([-2.0], [4.0]) ≈ [4.0]
 end
