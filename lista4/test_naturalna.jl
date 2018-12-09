a6c863944ef7eb870dc2323159bbf240d63d5610
Adrian Mucha <kestarumper8@gmail.com>
2018-12-09 16:51:02 +0100

    fix one test

--- lista4/test_naturalna.jl
+++ lista4/test_naturalna.jl
@@ -6,6 +6,9 @@ using Base.Test
 x = Float64[5, -7, -6, 0]
 fx = Float64[1, 2, 3, 4]
 @testset "Testy jednostkowe funkcji naturalna" begin
+    #1 + 2(x-5) + 3(x-5)(x+7) + 4(x-5)(x+7)(x+6) == -954 - 84x + 35x^2 + 4x^3
     @test Ilorazy.naturalna(x, fx) ≈ [-954.0, -84.0, 35.0, 4.0]
-    @test Ilorazy.naturalna([-2.0], [4.0]) ≈ [4.0]
+
+    #3 + 6(x-1) + 9(x-1)(x-2) == 15 - 21x + 9x^2
+    @test Ilorazy.naturalna([1.0, 2.0, 3.0], [3.0, 6.0, 9.0]) ≈ [15.0, -21.0, 9.0]
 end
