de2500c972f4fb0bb3e4b4a68429a30f2437626f
Adrian Mucha <kestarumper8@gmail.com>
2018-12-26 18:17:55 +0100

    move tests to separate file

--- lista5/Blocksys.jl
+++ lista5/Blocksys.jl
@@ -21,6 +21,7 @@ module Blocksys
     end
 
     function printSparse(array::SparseMatrixCSC{Float64, Int64}, n, p=1:n)
+		return
         idx = 1
         for i in p
             for j=1:n
@@ -168,81 +169,5 @@ module Blocksys
 			end
 		end
 		return backwardSubstitution(n, a, b, p, l)
-	end
-end
-
-using Blocksys
-using Base.Test
-println("")
-
-matFile_16_A = "$(pwd())/Dane16_1_1/A.txt"
-vecFile_16_b = "$(pwd())/Dane16_1_1/b.txt"
-ORIGINAL_A, n, l = loadMatFromFile(matFile_16_A)
-printSparse(ORIGINAL_A, n)
-ORIGINAL_b = loadVecFromFile(vecFile_16_b)
-
-# @testset "Standard Gauss" begin
-# 	@testset "Gaussian Elimination" begin
-# 		A = SparseMatrixCSC(Float64[
-# 			6 -2 2 4;
-# 			12 -8 6 10;
-# 			3 -13 9 3;
-# 			-6 4 1 -18
-# 		])
-# 		A_After = SparseMatrixCSC(Float64[
-# 			6 -2 2 4;
-# 			0 -4 2 2;
-# 			0 0 2 -5;
-# 			0 0 0 -3
-# 		])
-# 		b = Float64[12, 34, 27, -38]
-# 		b_After = Float64[12, 10, -9, -3]
-# 		A_computed, b_computed = gaussElimination(4, A, b)
-# 		@test A_computed ≈ A_After
-# 		@test b_computed ≈ b_After
-# 	end
-#
-# 	@testset "backwardSubstitution with triangleUpperMatrix" begin
-# 		A = SparseMatrixCSC(Float64[6 -2 2 4; 0 -4 2 2; 0 0 2 -5; 0 0 0 -3])
-# 		b = Float64[12, 10, -9, -3]
-# 		x = backwardSubstitution(4, A, b)
-# 		@test x ≈ Float64[1, -3, -2, 1]
-# 	end
-#
-# 	@testset "Solve with standard Gauss Elimination" begin
-# 		A, b, x = gaussElimination(n, ORIGINAL_A, ORIGINAL_b)
-# 		@test x ≈ ones(Float64, n)
-# 	end
-# end
-
-@testset "Normal" begin
-	@testset "Solve with Gaussian Elimination SPECIFIC" begin
-		A, b, p, x = gaussEliminationSpecific(n, ORIGINAL_A, ORIGINAL_b, l)
-		printSparse(A, n)
-		@test x ≈ ones(Float64, n)
-	end
-
-	@testset "Build LU matrix" begin
-		A, b, p, x = gaussEliminationSpecific(n, ORIGINAL_A, ORIGINAL_b, l, false, true)
-		printSparse(A, n, p)
-	end
-end
-
-@testset "With CHOICE" begin
-	@testset "Solve with Gaussian Elimination SPECIFIC with CHOICE" begin
-		A, b, p, x = gaussEliminationSpecific(n, ORIGINAL_A, ORIGINAL_b, l, true)
-		printSparse(A, n, p)
-		@test x ≈ ones(Float64, n)
-	end
-
-	@testset "Build LU matrix" begin
-		A, b, p, x = gaussEliminationSpecific(n, ORIGINAL_A, ORIGINAL_b, l, true, true)
-		printSparse(A, n, p)
-	end
-
-	@testset "Solve LU" begin
-		A, p = buildLU(n, ORIGINAL_A, l, true)
-		x = solveLU(n, A, ORIGINAL_b, l, p)
-		@test x ≈ ones(n)
 	end
 end
